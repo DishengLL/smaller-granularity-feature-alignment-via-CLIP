6363c7bf950340fa932081ca9a7c2558bde1a6d5
DishengLL <1554200903@qq.com>
2023-12-29 21:10:31 +0800

    12/29 uncertain based strategy weighting

--- main.py
+++ main.py
@@ -118,12 +118,16 @@ if __name__ == "__main__":
     parser.add_argument('--two_phases',action='store_true', default=False, help='implement 2-phases training scheme') 
     parser.add_argument('--no_orthogonize',action='store_true', default=False, help='do not implement orthogonization operation in the whole pipeline')
     parser.add_argument('--no_contrastive',action='store_true', default=False, help='do not implement contrastive alignment between text and images')  
+    parser.add_argument('--uncertain_based_weight', "-u", action='store_true', default=False, help='using uncertainty strategy to weight different sublosses(defualt: false)')  
     args = parser.parse_args()    
     backbone = "biomedclip" if args.backbone == None else args.backbone
     backbone_v = None if args.backbone_v == None else args.backbone_v
     prompt = "basic" if args.prompt == None else args.prompt
     visual_branch_only = args.vision_only
     two_phases = args.two_phases
+    uncertain_based_weight = args.uncertain_based_weight
+    if uncertain_based_weight:
+      print(constants.RED + "uning uncertain based strategy to weight different sublosses"+constants.RESET)
     if two_phases:
       print(constants.RED + "using two phase training scheme" + constants.RESET)
     no_orthogonize = args.no_orthogonize
@@ -155,8 +159,12 @@ if __name__ == "__main__":
         pin_memory=True,
         num_workers = num_of_thread,
         )
-
-    model = MultiTaskModel(nntype = backbone, visual_branch_only = visual_branch_only, backbone_v = backbone_v, high_order=high_order, no_orthogonize = no_orthogonize, no_contrastive=no_contrastive)
+    param_dict = {"weight_strategy": uncertain_based_weight}
+    # model definition
+    model = MultiTaskModel(nntype = backbone, visual_branch_only = visual_branch_only, backbone_v = backbone_v, 
+                           high_order=high_order, no_orthogonize = no_orthogonize, no_contrastive=no_contrastive,
+                           *param_dict)
+    # loss definition
     loss_model = LG_CLIP_LOSS(MultiTaskModel = model, learnable_weight=learnable_weight,).to(device)
 
     # build evaluator
