727ceb8cbe4f1ef293f954545057126f617587a9
DishengLL <1554200903@qq.com>
2024-01-02 11:24:26 +0800

    1/2 update readme, loss, main, res_analysis for task_difficulty_based weighting strategy.

--- main.py
+++ main.py
@@ -167,7 +167,7 @@ if __name__ == "__main__":
     # model definition
     model = MultiTaskModel(nntype = backbone, visual_branch_only = visual_branch_only, backbone_v = backbone_v,high_order=high_order, no_orthogonize = no_orthogonize, no_contrastive=no_contrastive, )
     # loss definition
-    loss_model = LG_CLIP_LOSS(MultiTaskModel = model, learnable_weight=learnable_weight, *param_dict).to(device)
+    loss_model = LG_CLIP_LOSS(MultiTaskModel = model, learnable_weight=learnable_weight, **param_dict).to(device)
 
     # build evaluator
     val_data = TestingDataset(backbone_type=backbone)
