d304cae9ca62dcb8dfdbad4ae1fcd957e1e48617
DishengLL <1554200903@qq.com>
2024-01-12 09:41:19 +0800

    1/12

--- models.py
+++ models.py
@@ -25,12 +25,10 @@ os.environ['CURL_CA_BUNDLE'] = ''
 import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
 
-
 device = "cuda" if torch.cuda.is_available() else "cpu"
 class OrthogonalTextEncoder(nn.Module):
     def __init__(self, d_model=512):
         super().__init__()
-        # Transformer编码器
         self.encoder_ly = nn.TransformerEncoderLayer(d_model, nhead=8, dim_feedforward=2048, dropout=0.1, batch_first=True)
         self.encoder = nn.TransformerEncoder(self.encoder_ly, num_layers = 8)
 
@@ -42,71 +40,9 @@ class OrthogonalTextEncoder(nn.Module):
         x = self.encoder(x)
         return x
 
-class ImgClassifier(nn.Module):
-    '''take LGCLIP model with linear heads for supervised classification on images -- positive/negative/uncertain.
-    '''
-    def __init__(self,
-        img_branch,
-        num_class:str,
-        input_dim=512,
-        mode='multiclass',
-        **kwargs) -> None:
-        '''args:
-        vision_model: the LGCLIP vision branch model that encodes input images into embeddings.
-        num_class: number of classes to predict
-        input_dim: the embedding dim before the linear output layer
-        mode: multilabel, multiclass, or binary
-        input number:  the number of input embeddings
-        num_class: corresponding with the number of disease --- the dim of output
-        '''
-        super(ImgClassifier, self).__init__()
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.model = img_branch.to(device)
-        self.num_class = num_class
-        assert mode.lower() in ['multiclass','multilabel','binary']
-        self.mode = mode.lower()
-        if num_class > 2:
-            if mode == 'multiclass':
-                self.loss_fn = nn.CrossEntropyLoss()
-            else:
-                self.loss_fn = nn.BCEWithLogitsLoss()
-
-            self.fc = nn.Linear(input_dim, input_dim)
-            self.cls = nn.Linear(input_dim, num_class)
-        else:
-            self.loss_fn = nn.BCEWithLogitsLoss()
-            self.fc = nn.Linear(input_dim, 1)
-
-    def forward(self,
-        img_path,  ## original image
-        labels=None,
-        return_loss=True,
-        **kwargs,
-        ):
-
-        assert labels is not None
-        
-        outputs = defaultdict()
-        image_embeddings = image_embeddings.cuda().to("cpu")
-        # take embeddings before the projection head
-        img_embeds = self.model(img_path)
-        logits = self.fc(img_embeds)
-        logits = self.cls(logits)
-        outputs['embedding'] = img_embeds
-        outputs['logits'] = logits
-        if labels is not None and return_loss:
-            labels = labels.cuda().float()
-            if len(labels.shape) == 1: labels = labels.view(-1,1)
-            if self.mode == 'multiclass': labels = labels.flatten().long()
-            loss = self.loss_fn(logits, labels)
-            outputs['loss_value'] = loss
-        return outputs
-
 class SplitVisEncoder(nn.Module):
     def __init__(self, n, d_model=512, nhead = 8, layers = 6, hid_dim=2048, drop = 0.01):
         super(SplitVisEncoder, self).__init__()
-
-        # 分割输入向量为n份
         self.n = n
         self.input_splits = nn.Linear(d_model, d_model * n)
         self.fc = nn.Linear(d_model * n, d_model * n)
@@ -114,7 +50,6 @@ class SplitVisEncoder(nn.Module):
             self.input_splits,
             self.fc
         )
-        # Transformer编码器
         self.encoder_ly = nn.TransformerEncoderLayer(d_model, nhead=8, dim_feedforward=2048, dropout=0.1, batch_first=True)
         self.encoder = nn.TransformerEncoder(self.encoder_ly, num_layers = 8)
 
@@ -126,20 +61,14 @@ class SplitVisEncoder(nn.Module):
         x = self.sequential(x) # 1 * n * dim
         x = x.view(x.size(0), self.n, -1)   #.permute(1, 0, 2)  # 调整形状为 (n, batch_size, d_model)
 
-        # 通过Transformer编码器
         x = self.encoder(x)
-
-        # 取每个时间步的输出
-        # x = x.permute(1, 0, 2)
 
         return x #.cuda()
 
 class TextBranch(nn.Module):
     def __init__(self, text_embedding_dim = 512, num_transformer_heads = 8, num_transformer_layers = 6, proj_bias = False, nntype = None):
         super().__init__()
-        # 初始化 CLIP 预训练模型和处理器
         self.projection_head = nn.Linear(512, 512, bias=False)
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
         if nntype == None:
             self.backbone = "clip"
         else:
@@ -149,13 +78,11 @@ class TextBranch(nn.Module):
         if self.backbone in ["biomed", "BiomedCLIP", "biomedclip"]:
             import open_clip
             self.clip_model, preprocess_train, preprocess_val = open_clip.create_model_and_transforms('hf-hub:microsoft/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224', device = device)
-            # self.tokenizer = open_clip.get_tokenizer('/public_bme/data/lds/model_zoo/biomed/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224')
         elif self.backbone == "custom":
             raise NotImplemented("has not implemented the custom backbone in text branch")
         else:
             ## the default backbone is CLIP -- text encoder
             self.clip_model, self.clip_processor  = clip.load("/public_bme/data/lds/model_zoo/ViT-B-32.pt", device=self.device)
-        # 冻结 CLIP 部分的参数
         if self.backbone != "custom":
           for param in self.clip_model.parameters():
               param.requires_grad = False
@@ -170,26 +97,20 @@ class TextBranch(nn.Module):
         mid: b x n x 512
         输出: b x [n vectors corresponding with prompts]
         '''
-        # 输入经过 CLIP 预训练模型
-        # text_inputs = torch.cat([clip.tokenize(f"image of {c}") for c in text_inputs]).to(device)
         text_features = []
-        # print(f'\033[31mthe type of text_inputs : {type(text_inputs)}\033[0m')
         if self.backbone != "custom":
           with torch.no_grad():
               for text_input in text_inputs:
-                text_feature = torch.load(text_input, map_location=self.device)
+                text_feature = torch.load(text_input, map_location=device)
                 text_features.append(text_feature)                  
-        text_features = torch.stack(text_features, dim = 0).squeeze().to(self.device)
-        output = self.transformer(text_features)  ## ToDo 正则化处理
+        text_features = torch.stack(text_features, dim = 0).squeeze().to(device)
+        output = self.transformer(text_features) 
         return  output
-
-      
+ 
 class ImgBranch(nn.Module):
     def __init__(self, text_embedding_dim = 512, num_transformer_heads = 8, num_transformer_layers = 6, proj_bia = False, nntype = None, backbone_v:str = None):
         super().__init__()
-        # 初始化 CLIP 预训练模型和处理器
         self.projection_head = nn.Linear(512, 512, bias=False)
-        device = "cuda" if torch.cuda.is_available() else "cpu"
         nlabel = len(_constants_.CHEXPERT_LABELS)
         
         self.VisEncoder = SplitVisEncoder(nlabel, d_model=512, nhead = 8, layers = 6, hid_dim=2048, drop = 0.01).to(device)
@@ -215,7 +136,7 @@ class ImgBranch(nn.Module):
             raise NotImplemented("using custom vis backbone which has not be defined!!!!!")
         else:
             self.clip_model, self.clip_processor  = clip.load("/public_bme/data/lds/model_zoo/ViT-B-32.pt", device=device)
-        #  in this case, Biomed and CLIP model are been frozen 
+        #  in this version, Biomed and CLIP model are been frozen 
         for param in self.clip_model.parameters():
             param.requires_grad = False
 
@@ -223,10 +144,8 @@ class ImgBranch(nn.Module):
     
     def densenet(self, n_dim = 512):
         model = models.densenet121(weights=None)
-        # 初始化一个空 dict
         model_dict = model.state_dict()
         pretrained_state = torch.load("/public_bme/data/lds/model_zoo/densenet/densenet121-a639ec97.pth")
-        # 修改 key
         new_state_dict = OrderedDict()
         for k, v in pretrained_state.items():
             if 'denseblock' in k:
@@ -237,9 +156,6 @@ class ImgBranch(nn.Module):
                 new_state_dict[k] = v
         
         model.load_state_dict(new_state_dict)
-
-
-        # model.load_state_dict(checkpoint)
 
         num_ftrs = model.classifier.in_features
         model.classifier = nn.Sequential(nn.Linear(num_ftrs, n_dim))  
@@ -252,21 +168,16 @@ class ImgBranch(nn.Module):
         '''
         images = []
         for image in image_path:
-            # if "/Users/liu/Desktop/school_academy/ShanghaiTech" in image:
-            #     image = image.replace("/Users/liu/Desktop/school_academy/ShanghaiTech", "D://exchange//ShanghaiTech//")
             images.append(torch.load(image))
 
-        image_input = torch.tensor(np.stack(images)).to(self.device)
-        # 输入经过 CLIP 预训练模型
+        image_input = torch.tensor(np.stack(images)).to(device)
         if self.backbone != "custom":
           with torch.no_grad():
               image_features = self.clip_model.encode_image(image_input).float()
         else:
-            # print("Using " + _constants_.RED + f"{self.backbone_n} as vision encoder" + _constants_.RESET + " in image branch" )
             image_features = self.backbone_v(image_input).float()
         output = self.VisEncoder(image_features)
-        return output
-    
+        return output 
 
 class CustomVisEncoder(nn.Module):
     def __init__(self):
@@ -345,10 +256,8 @@ class LGCLIP(nn.Module):
         print('load model weight from:', input_dir)
 
     def encode_text(self, inputs_text:list):
-        # inputs_text = inputs_text.cuda()
         text_embeds = self.text_model(inputs_text)    # text_feature: backbone generated; text_embedding: processed embeddings
         text_embeds = text_embeds / text_embeds.norm(dim=-1, keepdim=True)
-        # torch.save(text_feature, f'text_feature_backbone_{self.nntype}.pth')
         return text_embeds
 
     def encode_image(self, img_path=None):
@@ -361,8 +270,6 @@ class LGCLIP(nn.Module):
         self.logit_scale.data = torch.clamp(self.logit_scale.data, 0, 4.6052)
         logit_scale = self.logit_scale.exp()
         batch = img_emb.shape[0]
-        # reshaped_image_embedding = img_emb.view(batch, -1)
-        # reshaped_text_embedding =text_emb.view(batch, -1)
         reshaped_text_embedding = text_emb.squeeze()
         reshaped_image_embedding = img_emb.squeeze()
         if  (len(reshaped_image_embedding.shape) == len(reshaped_text_embedding.shape) == 2):
@@ -399,19 +306,19 @@ class LGCLIP(nn.Module):
             graph_align = False,
             **kwargs,
             ):
-            # input_text = input_text.cuda()/
             clip_loss = 0 # "no applicable in visual branch case"
             loss = 0
             graph_align_loss = 0
             text_embeds = 0
             logits_per_image = 0
-            img_embeds = self.encode_image(img_path).to(device)#.cuda()
+            img_embeds = self.encode_image(img_path).to(device)
             if eval:  # only need image embeddings for following process
               return {'img_embeds':img_embeds, 'text_embeds':text_embeds,
                 'logits_per_image':logits_per_image, 'loss_value':loss}
             if not self.visual_branch_only:
-              text_embeds = self.encode_text(input_text).to(device)#.cuda()
-              logits_per_image = self.compute_logits(img_embeds, text_embeds) #similarity matrix img2text [0, 1] in multibatch case: the outer matrix contain several inner matrix text-image
+              text_embeds = self.encode_text(input_text).to(device)
+              #similarity matrix img2text [0, 1] in multibatch case: the outer matrix contain several inner matrix text-image
+              logits_per_image = self.compute_logits(img_embeds, text_embeds) 
 
               if return_loss:
                   if not self.no_contrastive:
@@ -420,7 +327,8 @@ class LGCLIP(nn.Module):
                   if self.graph_align != "NA":
                     graph_alignment = Hier_graph_align(logits_per_image)
                     if self.graph_align == "binary":
-                      prior_graph_tensor = torch.load("./constants/normalized_cost_matrix.pt")    # using cost to represent correlation between different diseases (ps: in cost matrix, diagonal elements are 0)
+                      # using cost to represent correlation between different diseases (ps: in cost matrix, diagonal elements are 0)
+                      prior_graph_tensor = torch.load("./constants/normalized_cost_matrix.pt")   
                       graph_align_loss = graph_alignment.get_loss(prior_graph_tensor)
                       loss = clip_loss + graph_align_loss
             return {'img_embeds':img_embeds, 'text_embeds':text_embeds,
@@ -466,7 +374,6 @@ class PN_classifier(nn.Module):
         **kwargs
         ):
         outputs = defaultdict()
-        # img_embeddings = img_embeddings.cuda()
         # take embeddings before the projection head
         num_batch = img_embeddings.shape
         num_batch = num_batch[0]
@@ -480,13 +387,11 @@ class PN_classifier(nn.Module):
         assert img_label is not None
 
         if multi_CLS:
-            # self.lose_fn_overall = 
             raise NotImplemented("have not implemented")
 
         if img_label is not None and return_loss:
             if type(img_label[0]) is str:
                 nested_list = [json.loads(s) for s in img_label]
-            # print(nested_list)
             img_label = torch.tensor(np.stack(nested_list), dtype=torch.long).to(device)
             logits = logits.view(-1, self.num_cat)
             
@@ -501,29 +406,23 @@ class Orthogonal_dif(nn.Module):
     '''
     def __init__(self,
                  logit_scale_init_value = 0.07):
-        super().__init__()
-        # self.text_module = TextBranch()           
+        super().__init__()          
         # learnable temperature for contrastive loss
         self.logit_scale = nn.Parameter(torch.log(torch.tensor(1/logit_scale_init_value)))
 
-    
     def forward(self,
         text_embeds,
         return_loss=True,
         **kwargs,
         ):
-        # print("the shape if text_embedding: ",text_embeds.shape)
         loss = 0
-        # batch, num_cls, _ = text_embeds.shape
         _len_ = len(text_embeds.shape)
         multi_logits_per_text = []
-        # logits_per_text = self.compute_logits(text_embeds, text_embeds) #similarity matrix text2text [0, 1]
 
         if _len_ == 2:  ## just one sample
           if return_loss:
               logits_per_text =  self.compute_logits(text_embeds)
               loss = self.contrastive_loss(logits_per_text)
-              # loss = loss + self.contrastive_loss(logits_per_text.T)
           return {'text_embeds':text_embeds,
                   'loss_value':loss, 
                   'multi_logits_per_text':logits_per_text}
@@ -534,7 +433,6 @@ class Orthogonal_dif(nn.Module):
                 logits_each_sample = self.compute_logits(sample)
                 multi_logits_per_text.append(logits_each_sample)
                 loss = loss + self.contrastive_loss(logits_each_sample)
-                # loss = loss + self.contrastive_loss(logits_each_sample.T)
               multi_logits = torch.stack(multi_logits_per_text, dim = 0)
               loss = loss / n_text_sample
           return {'text_embeds':text_embeds,
@@ -545,9 +443,7 @@ class Orthogonal_dif(nn.Module):
         self.logit_scale.data = torch.clamp(self.logit_scale.data, 0, 4.6052)
         logit_scale = self.logit_scale.exp()
         logits_per_text = torch.matmul(emb, emb.t()) * logit_scale
-        # logits_per_text /= logits_per_text.norm(dim=-1, keepdim=True)
         return logits_per_text.t()
-
 
     def contrastive_loss(self, logits: torch.Tensor) -> torch.Tensor:
         return nn.functional.cross_entropy(logits, torch.arange(len(logits), device=logits.device))
@@ -624,13 +520,11 @@ class Hier_graph_align():
       tot_cos_dis = tot_cos_dis + (1 - (each_sample_cos_distance/n_disease))
     avg_tot_cos_dis = tot_cos_dis / batch_size
     return avg_tot_cos_dis
-    
 
 class MultiTaskModel(nn.Module):
-    def __init__(self, nntype = "clip", visual_branch_only = False, backbone_v = None, high_order="NA", no_orthogonize = False, no_contrastive = False, **kwargs):
+    def __init__(self, nntype = "clip", visual_branch_only = False, backbone_v = None, high_order="NA", 
+                 no_orthogonize = False, no_contrastive = False, **kwargs):
         super().__init__()
-        # print(_constants_.BLUE+"the current backbone nn is: "+_constants_.RESET+nntype)
-        # CLIP fashion alignment
         param_dict = kwargs
         self.uncertain_based_weight = param_dict['weight_strategy'] if "weight_strategy" in param_dict else False
         if  (nntype not in ["clip", "biomedclip", "custom"]):
