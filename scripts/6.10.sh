05729f926e5369a826ab4adf682af4ddc9f773e6
DishengLL <1554200903@qq.com>
2024-06-13 00:08:33 +0800

    update

--- scripts/6.10.sh
+++ scripts/6.10.sh
@@ -14,7 +14,13 @@
 # python ../src/main.py --backbone biomedclip  --trainable_VisionEncoder  --AP-PA-view  --labeling_strategy S1 --backbone biomedclip  --high_order binary >> ../src/6_10/6.10_aligment_binary_cross_entropy.log
 
 
-echo "training with aligment -- binary cross-entropy loss + task balanced"
-python ../src/main.py --backbone biomedclip  --trainable_VisionEncoder  --AP-PA-view  --labeling_strategy S1 --backbone biomedclip  --high_order binary --weight_strategy task_balance >> ../src/6_10/6.10_aligment_binary_cross_entropy_taskBalanced.log
+# echo "training with aligment -- binary cross-entropy loss + task balanced"
+# python ../src/main.py --backbone biomedclip  --trainable_VisionEncoder  --AP-PA-view  --labeling_strategy S1 --backbone biomedclip  --high_order binary --weight_strategy task_balance >> ../src/6_10/6.10_aligment_binary_cross_entropy_taskBalanced.log
 
 
+echo "training with aligment -- binary cross-entropy loss + uncertainty"
+python ../src/main.py --backbone biomedclip  --trainable_VisionEncoder  --AP-PA-view  --labeling_strategy S1 --backbone biomedclip  --high_order binary --weight_strategy uncertain_based_weight >> ../src/6_11/6.11_aligment_binary_cross_entropy_uncertainty.log
+
+
+echo "training with aligment -- focal loss + uncertainty"
+python ../src/main.py --backbone biomedclip  --trainable_VisionEncoder  --AP-PA-view  --labeling_strategy S1 --backbone biomedclip  --high_order binary --weight_strategy uncertain_based_weight  --focal_loss >> ../src/6_11/6.11_aligment_focal_loss_uncertainty.log
--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,3 @@
 # from . import health_multimodal
 from .PLM_embeddings import PLM_embedding
+# import PLM_embeddings
